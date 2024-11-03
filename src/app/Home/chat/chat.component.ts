bc80cbb00b1b7fde9dd892775c939e62d308d007
Miguel Angel Gtz Gomez <233382@ids.upchiapas.edu.mx>
2024-11-03 10:24:40 -0600

    update dependences

--- src/app/Home/chat/chat.component.ts
+++ src/app/Home/chat/chat.component.ts
@@ -1,82 +1,85 @@
-import { Component, DoCheck, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Mensaje } from '../../Interfaces/mensaje';
 import { ChatsService } from '../../Services/chats.service';
+
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrl: './chat.component.css'
+  styleUrls: ['./chat.component.css']
 })
-export class ChatComponent implements OnInit, DoCheck, OnDestroy {
+export class ChatComponent implements OnInit, OnDestroy {
+  constructor(public _mensaje: ChatsService) {}
+  
+  usuarioActivo: any = {
+    email: "",
+    nombre: "",
+    idUsuario: 0,
+    password: ""
+  };
 
-  usuarioActivo : any={
-    email:"",
-    nombre :"",
-    idUsuario:0,
-    password : ""
-  }
-  flag = true;
-  constructor(public _mensaje: ChatsService){
-  }
-  mensajeDelChat : Mensaje[] = [
-  ]
-  user : number = Number(sessionStorage.getItem('id_user')) || 0
-  ngOnInit(): void {
-    this._mensaje.getMensajesById(Number(sessionStorage.getItem("id_chat"))).subscribe(data=>{
-    this.mensajeDelChat=data
-      this._mensaje.mensaje = data
-      console.log("datos cargados", this._mensaje.mensaje)
-    })
-    this._mensaje.getPerteneceTO(Number(sessionStorage.getItem("id_chat"))).subscribe(data=>{
-      this._mensaje.usuarios = data
-      data.forEach(element => {
-        if(element.Usuario_idUsuario != this.user){
-          this._mensaje.getChatsById(element.Usuario_idUsuario).subscribe(data=>{
-            this.usuarioActivo = data
-            console.log(data)
-          })
-        }
-      })
-      console.log("usuarios en el chat",data)
-    })
-  }
-  ngDoCheck(): void {
-      this.contador++
-  }
-  contador = 0
   @Input() msj = {
+    hora: "",
+    fecha: "",
+    estatus: false,
+    id_usuario: sessionStorage.getItem("id_user"),
+    id_chat: sessionStorage.getItem("id_chat") || 0,
+    mensaje: ""
+  };
+  
+  mensajeDelChat: Mensaje[] = [];
+  user: number = Number(sessionStorage.getItem('id_user')) || 0;
+
+  ngOnInit(): void {
+    this._mensaje.getMensajesById(Number(sessionStorage.getItem("id_chat"))).subscribe(data => {
+      this.mensajeDelChat = data;
+      this._mensaje.mensaje = data;
+      console.log("datos cargados", this._mensaje.mensaje);
+    });
+    
+    this._mensaje.getPerteneceTO(Number(sessionStorage.getItem("id_chat"))).subscribe(data => {
+      this._mensaje.usuarios = data;
+      data.forEach(element => {
+        if (element.Usuario_idUsuario != this.user) {
+          this._mensaje.getChatsById(element.Usuario_idUsuario).subscribe(data => {
+            this.usuarioActivo = data;
+            console.log(data);
+          });
+        }
+      });
+      console.log("usuarios en el chat", data);
+    });
+
+    // Escucha de mensajes entrantes
+    this._mensaje.getMessages().subscribe((msg: any) => {
+      this.mensajeDelChat.push(msg);
+    });
+  }
+
+  enviarMensaje() {
+    const date = new Date();
+    const hora = (date.getHours() + ":" + date.getMinutes());
+    this.msj.hora = hora;
+
+    this._mensaje.addMensaje(this.msj).subscribe(data => {
+      console.log("mensaje añadido", data);
+      this._mensaje.updateChat(Number(sessionStorage.getItem("id_chat")), {"ultimo_msj": data.mensaje }).subscribe(data => {
+        console.log("chat actualizado", data);
+      });
+    }, error => {
+      console.error('Error al agregar mensaje:', error);
+    });
+
+    this.msj = {
       hora: "",
       fecha: "",
       estatus: false,
       id_usuario: sessionStorage.getItem("id_user"),
       id_chat: sessionStorage.getItem("id_chat") || 0,
       mensaje: ""
-    }
-    enviarMensaje(){
-      const date  = new Date()
-      const hora = (date.getHours()+ ":" + date.getMinutes())
-      this.msj.hora = hora
-      this._mensaje.addMensaje(this.msj).subscribe(data => {
-        console.log("mensaje añadido" ,data)
-        this._mensaje.updateChat(Number(sessionStorage.getItem("id_chat")), {"ultimo_msj": data.mensaje }).subscribe(data=>{
-          console.log("chat actualizado", data)
-        })
-      },
-      error => {
-        console.error('Error al agregar mensaje:', error);
-      })
-      
-      this.msj = {
-        hora: "",
-        fecha: "",
-        estatus: false,
-        id_usuario: sessionStorage.getItem("id_user "),
-        id_chat: sessionStorage.getItem("id_chat") || 0,
-        mensaje: ""
-      }
-    }
-  
-    ngOnDestroy(): void {
-        sessionStorage.removeItem('id_chat')
-    }
+    };
+  }
 
+  ngOnDestroy(): void {
+    sessionStorage.removeItem('id_chat');
+  }
 }
--- src/app/Services/chats.service.ts
+++ src/app/Services/chats.service.ts
@@ -3,39 +3,57 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Usuario } from '../Interfaces/contacto';
 import { Mensaje } from '../Interfaces/mensaje';
+import { io, Socket } from 'socket.io-client';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatsService {
 
-  mensaje : Mensaje[] = [
-  ]
-
-  usuarios: Usuario[]=[]
+  mensaje : Mensaje[] = []
+  usuarios: Usuario[] = []
   private apiUrl = 'http://localhost:3000/api/mensajes/';
   private apiUrl2 = 'http://localhost:3000/api/usuarios';
   private apiUrlPert = "http://localhost:3000/api/uschat/usuario/"
-  private updChat="http://localhost:3000/api/chats/"
-  constructor(private http: HttpClient) { }
-
+  private updChat = "http://localhost:3000/api/chats/"
   
-  getMensajes(): Observable<Mensaje[]>{
-    return this.http.get<Mensaje[]>(this.apiUrl)
+  private socket: Socket;
+
+  constructor(private http: HttpClient) { 
+    this.socket = io('http://localhost:3000'); // URL del servidor Socket.IO
   }
-  getMensajesById(id:number): Observable<Mensaje[]>{
-    return this.http.get<Mensaje[]>(this.apiUrl + id)
+
+  getMensajes(): Observable<Mensaje[]> {
+    return this.http.get<Mensaje[]>(this.apiUrl);
   }
-  getChatsById(id: number): Observable<Mensaje>{
-    return this.http.get<Mensaje>(this.apiUrl2 + "/" + id)
+
+  getMensajesById(id: number): Observable<Mensaje[]> {
+    return this.http.get<Mensaje[]>(this.apiUrl + id);
   }
-  addMensaje(mensaje: any): Observable<any>{
+
+  getChatsById(id: number): Observable<Mensaje> {
+    return this.http.get<Mensaje>(this.apiUrl2 + "/" + id);
+  }
+
+  addMensaje(mensaje: any): Observable<any> {
     return this.http.post<Mensaje>(this.apiUrl, mensaje);
   }
-  updateChat(id:number, data:any): Observable<any>{
+
+  updateChat(id: number, data: any): Observable<any> {
     return this.http.put<Mensaje>(this.updChat + id, data);
   }
-  getPerteneceTO(id : number): Observable<any[]>{
-    return this.http.get<any[]>(this.apiUrlPert + id + "/pertenece")
+
+  getPerteneceTO(id: number): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrlPert + id + "/pertenece");
+  }
+
+  sendMessage(msg: string) {
+    this.socket.emit('mensaje', msg);
+  }
+
+  getMessages(): Observable<string> {
+    return new Observable<string>((observer) => {
+      this.socket.on('mensaje', (msg: string) => observer.next(msg));
+    });
   }
 }
